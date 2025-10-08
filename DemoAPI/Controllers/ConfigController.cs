3f2a74cefa5ae6e03ab86859fdceceaa4b4e1e8c
aereo <aereo@PC1-ROOM409>
2025-10-08 15:27:16 +0500

    add configuration

---
+++ DemoAPI/Controllers/ConfigController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public ConfigController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var connstr = _config["ConnectionStrings:PostgreConnection"];
+            var appName = _config["AppSettings:AppName"];
+            var key = _config["AppSettings:APIKey"];
+            return Ok(new {connstr, appName, key });
+        }
+    }
+}
