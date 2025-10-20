844f50220ab78ec8cbce25ed6453e8bd5480eb1f
Марс Тазеев <marstazeev0@yandex.ru>
2025-10-20 17:32:08 +0500

    Тазеев 4пк2

--- DemoAPI/Models/DTO/CreateBookDTO.cs
+++ DemoAPI/Models/DTO/CreateBookDTO.cs
@@ -1,9 +1,15 @@
-﻿namespace DemoAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+namespace DemoAPI.Models.DTO
 {
     public class CreateBookDTO
     {
+        [Required(ErrorMessage = "Название книги обязательно")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Название должно быть от 1 до 200 символов")]
         public string Title { get; set; } = null!;
+
+        [Range(1, int.MaxValue, ErrorMessage = "ID автора должен быть положительным числом")]
         public int? AuthorId { get; set; }
+
         //ссылка на DTO автора
         public CreateAuthorDTO? newAuthor { get; set; }
     }
